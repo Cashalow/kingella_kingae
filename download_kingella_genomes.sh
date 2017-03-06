65ecc26ef73e666478e3f3819631931096923bd5
Cashalow <sachalau@gmail.com>
2017-03-06 11:30:54 +0100

    Update download_kingella_genomes.sh

--- download_kingella_genomes.sh
+++ download_kingella_genomes.sh
@@ -1,1 +1,5 @@
+kingella_genome_list_adress="https://www.ncbi.nlm.nih.gov/genomes/Genome2BE/genome2srv.cgi?action=download&orgn=%22Kingella%20kingae%22[orgn]&status=50|30|20&report=proks&group=--%20All%20Prokaryotes%20--&subgroup=--%20All%20Prokaryotes%20--&format="
+
+wget $kingella_genome_list_adress -o kingella_genome_list.txt
+cut -f19 kigella_genome_list.txt | sed -s "s/\(.*\)\/\(.*\)$/\1\/\2\/\2_genomic.fna.gz/"
 
