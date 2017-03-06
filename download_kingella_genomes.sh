5bad63b24f2377ed3f7a8cc72470be8809752c89
Cashalow <sachalau@gmail.com>
2017-03-06 16:07:56 +0100

    corrected the formatting command

--- download_kingella_genomes.sh
+++ download_kingella_genomes.sh
@@ -1,7 +1,8 @@
 kingella_genome_list_adress="https://www.ncbi.nlm.nih.gov/genomes/Genome2BE/genome2srv.cgi?action=download&orgn=%22Kingella%20kingae%22[orgn]&status=50|30|20&report=proks&group=--%20All%20Prokaryotes%20--&subgroup=--%20All%20Prokaryotes%20--&format="
 
 wget $kingella_genome_list_adress -O kingella_genome_list.txt
-tail -n +2 kingella_genome_list.txt | cut -f20 | sed -s "s/\(.*\)\/\(.*\)$/\1\/\2\/\2_genomic.fna.gz/" > kingella_genome_adresses.txt
+
+tail -n +2 kingella_genome_list.txt | cut -f20 | sed -s "s/\(.*\)\/\(.*\)$/\1\/\2\/\2\_genomic.fna.gz/" | sed -s "s/\r//g" > kingella_genome_adresses.txt
 
 mkdir raw_genome_sequences
 cd raw_genomde_sequences
