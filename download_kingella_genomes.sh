0603b6404889c6d4ed673c475c7b08e8e84f84f8
Cashalow <sachalau@gmail.com>
2017-03-06 11:35:20 +0100

    update for downloading genomes
    adding html link and processing for finding the adresses of the files

--- download_kingella_genomes.sh
+++ download_kingella_genomes.sh
@@ -1,5 +1,6 @@
 kingella_genome_list_adress="https://www.ncbi.nlm.nih.gov/genomes/Genome2BE/genome2srv.cgi?action=download&orgn=%22Kingella%20kingae%22[orgn]&status=50|30|20&report=proks&group=--%20All%20Prokaryotes%20--&subgroup=--%20All%20Prokaryotes%20--&format="
 
 wget $kingella_genome_list_adress -o kingella_genome_list.txt
-cut -f19 kigella_genome_list.txt | sed -s "s/\(.*\)\/\(.*\)$/\1\/\2\/\2_genomic.fna.gz/"
+cut -f19 kigella_genome_list.txt | sed -s "s/\(.*\)\/\(.*\)$/\1\/\2\/\2_genomic.fna.gz/" > kingella_genome_adresses.txt
+
 
