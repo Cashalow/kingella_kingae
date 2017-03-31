379f7942c7fc55d2a02b513b905d4878980354c5
Cashalow <sachalau@gmail.com>
2017-03-31 12:17:06 +0200

    rename output name of the concatenated genes

--- concatenate_single_line_alignments.sh
+++ concatenate_single_line_alignments.sh
@@ -1,4 +1,4 @@
 target_folder=$1
 
-paste -d'\0' ${target_folder}/*_oneline | sed "s/>\([0-9]\+\)>.*/>\1/"  | sed "/^\s*$/d" > core_genes_concatenated.fa
+paste -d'\0' ${target_folder}/*_oneline | sed "s/>\([0-9]\+\)>.*/>\1/"  | sed "/^\s*$/d" > concatenated_genes.fa
 
