cf23d16af25356d5a4cba11e8cfd2d6f4cc8aa2f
Cashalow <sachalau@gmail.com>
2017-03-31 11:02:43 +0200

    adding file for calculating average identity of an alignment using BioPython

---
+++ calculate_average_identity.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import Bio
+import Bio.SeqIO
+import Bio.AlignIO
+import sys
+
+def read_alignment(filename):
+    return(Bio.AlignIO.parse(filename, "fasta"))
+
+
+
+def return_seqRec(aliRec):
+    seqs={}
+    for i in aliRec:
+        for j in i:
+            seqs[j.id]=j.seq
+    return(seqs)
+
+def return_avg_identity(seqRec):
+    print("\t"+"\t".join(seqRec.keys()))
+    for i in seqRec.keys():
+        s=i+"\t"
+        for j in seqRec.keys():
+            matches=sum(aa1 == aa2 and aa1 != "-" for aa1, aa2 in zip(seqRec[i], seqRec[j]))
+            unmatches=sum(aa1 == aa2 and aa1 == "-" for aa1, aa2 in zip(seqRec[i], seqRec[j]))
+            s+=str(matches/float(len(seqRec[j])-unmatches))+"\t"
+        print(s)
+        
+
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+    return_avg_identity(return_seqRec(read_alignment(sys.argv[1])))
