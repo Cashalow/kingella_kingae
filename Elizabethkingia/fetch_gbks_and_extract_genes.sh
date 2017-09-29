9631a56fb25bc24e2ca725d66d9a71403fb92370
Cashalow <sachalau@gmail.com>
2017-09-29 16:59:18 +0200

    fix gbk

--- Elizabethkingia/fetch_gbks_and_extract_genes.sh
+++ Elizabethkingia/fetch_gbks_and_extract_genes.sh
@@ -1,6 +1,6 @@
-esearch -db assembly -query "Elizabethkingia miricola|Elizabethkingia meningoseptica|Elizabethkingia anophelis" | efetch -db assembly -format docsum | xtract -pattern DocumentSummary -if RefSeq -element SpeciesName FtpPath_RefSeq > refseq_ftp.txt
-cut -f 2 refseq_genomes.txt  | sed "s/\(\/GCF_.*\)/\1\1_genomic.gbff.gz/" > refseq_genbanks.txt
+esearch -db assembly -query "Elizabethkingia" | efetch -db assembly -format docsum | xtract -pattern DocumentSummary -if RefSeq -element SpeciesName FtpPath_RefSeq > refseq_ftp.txt
+cut -f 2 refseq_ftp.txt  | sed "s/\(\/GCF_.*\)/\1\1_genomic.gbff.gz/" > refseq_genbanks.txt
 mkdir -p gbks && cd gbks && wget -i ../refseq_genbanks.txt
-gzip -d *
+gzip -d *.gz
 mkdir -p fasta && cd fasta 
 rm -f *.fasta && for i in $(ls ../*.gbff); do python ../../../general_scripts/extract_gbk.py ${i}; done
