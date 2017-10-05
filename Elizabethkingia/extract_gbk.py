868df2e933e9f9672d9c6559382f78361b300c88
Cashalow <sachalau@gmail.com>
2017-10-05 11:55:41 +0200

    modif elizabethkingia

--- Elizabethkingia/extract_gbk.py
+++ Elizabethkingia/extract_gbk.py
@@ -1,16 +1,16 @@
 from Bio import SeqIO
 import sys
 
-products=["DNA gyrase subunit A", "DNA gyrase subunit B", "DNA topoisomerase (ATP-hydrolyzing) subunit B", "DNA topoisomerase IV subunit B"]
-genes = ["gyrA", "gyrB", "parE"]
+products=["DNA gyrase subunit A", "DNA gyrase subunit B", "DNA topoisomerase (ATP-hydrolyzing) subunit B", "DNA topoisomerase IV subunit B", "DNA topoisomerase IV subunit A"]
 
-corres={"DNA gyrase subunit A":"gyrA", "DNA gyrase subunit B":"gyrB", "DNA topoisomerase (ATP-hydrolyzing) subunit B": "gyrB", "DNA topoisomerase IV subunit B":"parE"}
+genes = ["gyrA", "gyrB", "parE", "parC"]
+
+corres={"DNA gyrase subunit A":"gyrA", "DNA gyrase subunit B":"gyrB", "DNA topoisomerase (ATP-hydrolyzing) subunit B": "gyrB", "DNA topoisomerase IV subunit B":"parE", "DNA topoisomerase IV subunit A":"parC"}
 
 def extract_translation(filename, gen, prod, corr):
     for record in SeqIO.parse(filename, "genbank"):
         for f in record.features:
             if f.type=="CDS":
-#                print(f.qualifiers["product"])
                 try:
                     if f.qualifiers["gene"][0] in gen:
                         with open(f.qualifiers["gene"][0]+".fasta", "a") as myfile:
--- Elizabethkingia/fetch_gbks_and_extract_genes.sh
+++ Elizabethkingia/fetch_gbks_and_extract_genes.sh
@@ -1,6 +1,17 @@
+#/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 esearch -db assembly -query "Elizabethkingia" | efetch -db assembly -format docsum | xtract -pattern DocumentSummary -if RefSeq -element SpeciesName FtpPath_RefSeq > refseq_ftp.txt
 cut -f 2 refseq_ftp.txt  | sed "s/\(\/GCF_.*\)/\1\1_genomic.gbff.gz/" > refseq_genbanks.txt
-mkdir -p gbks && cd gbks && wget -i ../refseq_genbanks.txt
-gzip -d *.gz
+mkdir -p gbks && cd gbks
+while read p;
+do
+    b=$(basename ${p})
+    if [ ! -f ${b%.gz}  ]
+    then
+	wget ${p}
+	gzip -f -d ${b}
+    fi
+done < ../refseq_genbanks.txt
 mkdir -p fasta && cd fasta 
-rm -f *.fasta && for i in $(ls ../*.gbff); do python ../../../general_scripts/extract_gbk.py ${i}; done
+rm -f *.fasta && for i in $(ls ../*.gbff); do python ${DIR}/extract_gbk.py ${i}; done
